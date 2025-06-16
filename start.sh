41755b24d397d0b736281c61e8b4bfa485bc005d
Ethan Triem <EJTriem@outlook.com>
2025-06-16 10:31:11 -0500

    werid error in docker trying to fix

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Running spy_dia_qqq_comp.py..."
-python scripts/spy_dia_qqq_comp.py
+#echo "Running spy_dia_qqq_comp.py..."
+#python scripts/spy_dia_qqq_comp.py
 
-echo "Running scrape.py..."
-python scripts/scrape.py
+#echo "Running scrape.py..."
+#python scripts/scrape.py
 
 echo "Running scrape.py..."
 python scripts/sent_count_sum.py
